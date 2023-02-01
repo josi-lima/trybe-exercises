ce9502bc5888d637dd8e631d5ac0482fec44fec5
josi-lima <teacher.josi@yahoo.com>
2023-02-01 00:35:14 -0300

    :recycle: refactor: Refactor code and add new solutions

--- PROJECTS/B3- Playground Functions/script.js
+++ PROJECTS/B3- Playground Functions/script.js
@@ -1,16 +1,21 @@
-// 💡 Desafio 1
-const compareTrue = (arg1, arg2) => arg1 && arg2 ? 'true' : 'false';
+// 💡 Desafio 1 - Crie a função compareTrue -----------------
+
+const compareTrue = (arg1, arg2) => {
+  if (arg1 && arg2) return true;
+  return false;
+};
 console.log(compareTrue(true, true));  // true
 
 // =================================================================
 
-// 🚀 Desafio 2
-const splitSentence = phrase => phrase.split(' ');
+// 💡 Desafio 2 - Crie a função splitSentence -------------------
+
+const splitSentence = (phrase) => phrase.split(' ');
 console.log(splitSentence('you gotta leave'));  // ['you', 'gotta', 'leave']
 
 // =================================================================
 
-// 💡 Desafio 3
+// 💡 Desafio 3 - Crie a função concatName -----------------------
 const concatName = (names) => {
   return names.at(-1), names[0];
 }
@@ -18,7 +23,7 @@ console.log(concatName(['My Mom', 'My Dad', 'Frida Kahlo', 'Anne Frank', 'Florbe
 
 // =================================================================
 
-// 🚀 Desafio 4
+// 💡 Desafio 4 - Crie a função footballPoints --------------------
 const footballPoints = function (wins, ties) {
   let gamesResult = wins * 3 + ties;
   return gamesResult;
@@ -27,7 +32,7 @@ console.log(footballPoints(3, 3));  // 12
 
 // =================================================================
 
-// 💡 Desafio 5
+// 💡 Desafio 5 - Crie a função highestCount ---------------------
 const highestCount = function (nums) {
   let maxNumber = nums[0];
   let counter = 0;
@@ -44,7 +49,7 @@ console.log(highestCount([2, 8, 7, 5, 8]));  // 2
 
 // =================================================================
 
-// 🚀 Desafio 6
+// 💡 Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas -----------------
 
 const calcTriangleArea = (base, height) => (base * height) / 2;
 console.log(calcTriangleArea(10, 15));
@@ -53,17 +58,18 @@ const calcRectangleArea = (base, height) => base * height;
 console.log(calcRectangleArea(10, 15));
 
 const calcAllAreas = (base, height, form) => {
-  if (form === 'triangle') {
-    return `O valor da área do triângulo é de: ${(base * height) / 2}.`;
-  } if (form === 'rectangle') {
-    return `O valor da área do retângulo é de: ${base * height}.`;
+  if (form === 'triângulo') {
+    return `O valor da área do triângulo é de: ${calcTriangleArea(base, height)}`;
+  } if (form === 'retângulo') {
+    return `O valor da área do retângulo é de: ${calcRectangleArea(base, height)}`;
   } return 'Não foi possível fazer o cálculo, insira uma forma geométrica válida.';
 }
-console.log(calcAllAreas(10, 15, 'triangle'));
+console.log(calcAllAreas(10, 15, 'triângulo'));
 
 // =================================================================
 
-// 💡 Desafio 7
+// 💡 Desafio 7 - Crie a função catAndMouse -----------------
+
 const catAndMouse = function (mouse, cat1, cat2) {
   let distanceCat1 = Math.abs(mouse - cat1);
   let distanceCat2 = Math.abs(mouse - cat2);
@@ -72,12 +78,12 @@ const catAndMouse = function (mouse, cat1, cat2) {
   } if (distanceCat2 < distanceCat1) {
     return 'cat2';
   } return 'os gatos trombam e o rato foge';
-}
+};
 console.log(catAndMouse(5, 4, 2));  // cat1
 
 // =================================================================
 
-// 🚀 Desafio 8
+// 🚀 Desafio 8 - Crie a função fizzBuzz ----------------------
 
 const fizzBuzz = function (numbers) {
   const message = numbers.map((num) => {
@@ -91,32 +97,32 @@ console.log(fizzBuzz([2, 15, 7, 9, 45]));  // ['bug!', 'fizzBuzz', 'bug!', 'fizz
 
 // =================================================================
 
-// 💡 Desafio 9
+// 💡 Desafio 9 - Crie a função encode e a função decode ============================================
+// Reference: https://stackoverflow.com/questions/16576983/replace-multiple-characters-in-one-replace-call
 
 const encode = function (string) {
   const chars = { a: 1, e: 2, i: 3, o: 4, u: 5 };
-  string = string.replace(/[aeiou]/g, m => chars[m]);
+  string = string.replace(/[aeiou]/g, (m) => chars[m]);
   return string;
-}
-console.log(encode('hi there!'));  // h3 th2r2!
+};
+console.log(encode('hi there!')); // h3 th2r2!
 
 const decode = function (string) {
   const chars = { 1: 'a', 2: 'e', 3: 'i', 4: 'o', 5: 'u' };
-  string = string.replace(/[12345]/g, m => chars[m]);
+  string = string.replace(/[12345]/g, (m) => chars[m]);
   return string;
-}
+};
 console.log(decode('h3 th2r2!'));  // hi there!
-
-// Reference: https://stackoverflow.com/questions/16576983/replace-multiple-characters-in-one-replace-call
 
 // =================================================================
 
-// Desafio 10
+// 💡 Desafio 10 - Crie a função techList ---------------------------
+
 const techList = function (techs, names) {
   techs.sort();
   let result = [];
   if (techs.length === 0) {
-    return 'Vazio!';
+    return [];
   }
   for (let index = 0; index < techs.length; index += 1) {
     result.push({ tech: techs[index], name: names });
@@ -137,54 +143,60 @@ console.table(techList(['React', 'Jest', 'HTML', 'Jest', 'CSS', 'JavaScript'], '
 └─────────┴──────────────┴─────────┘ */
 // =================================================================
 
-// 🌱 Desafio 11
-function generatePhoneNumber(arrayNumbers) {
+// 🌱 Desafio 11 - Crie a função generatePhoneNumber --------------------------
 
+const generatePhoneNumber = (arrayNumbers) => {
   let error = 'não é possível gerar um número de telefone com esses valores';
   let phoneNumbers = arrayNumbers.join('');
   let ddd = phoneNumbers.substring(0, 2);
   let firstFiveNumbers = phoneNumbers.substring(2, 7);
   let lastFourNumbers = phoneNumbers.substring(7, 11);
-  
-  if (arrayNumbers.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  } 
-  if ((arrayNumbers.some(p => p > 9)) || (arrayNumbers.some(p => p < 0))) {
+
+  if (arrayNumbers.length !== 11) return 'Array com tamanho incorreto.';
+  if ((arrayNumbers.some((p) => (p > 9))) || (arrayNumbers.some((p) => (p < 0)))) {
     return error;
   }
-  for (let index = 0; index < arrayNumbers.length; index += 1 ) {
-    let result = arrayNumbers[index];
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
     let count = 0;
     for (let index2 = 0; index2 < arrayNumbers.length; index2 += 1) {
-      if(result === arrayNumbers[index2]){
-        count += 1;
+      if (arrayNumbers[index] === arrayNumbers[index2]) count += 1;
     }
+    if (count > 2) return error;
   }
-    if (count > 2) {
-      return error;
-    }
-}
-  let phoneNumber = '('.concat(ddd + ')' + ' ' + firstFiveNumbers + '-' + lastFourNumbers);
-  return phoneNumber;
-}
+  return `(${ddd}) ${firstFiveNumbers}-${lastFourNumbers}`;
+};
 
 // =================================================================
 
-// 🌱 Desafio 12
-function triangleCheck(a, b, c) {
-  if (a > b + c || b > a + c || (c > a + b && a > Math.abs(b + c))
-  || (b > Math.abs(a + c)) || (c > Math.abs(a + b))
-  ) {
-    return false;
-  }
+// 🌱 Desafio 12 -  Crie a função triangleCheck ---------------------
+
+const triangleCheck = (a, b, c) => {
+  if ((a > Math.abs(b + c))) return false;
+  if ((b > Math.abs(a + c))) return false;
+  if ((c > Math.abs(a + b))) return false;
   return true;
-}
+};
+console.log(triangleCheck(150, 40, 60));
 
 // =================================================================
 
-// 🌱 Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+// 🌱 Desafio 13 - Crie a função hydrate -------------------------------
+
+const hydrate = function (drinks) {
+  let quantity = /\d+/g;
+  let drinksQuantity = drinks.match(quantity);
+  let result = 0;
+
+  for (let index = 0; index < drinksQuantity.length; index += 1) {
+    result += parseFloat(drinksQuantity[index]);
+  }
+  if (result > 1 && result <= 9) {
+    return `${result} copos de água`;
+  } if (result === 1) {
+    return `${result} copo de água`;
+  }
+};
+console.log(hydrate('1 cerveja, 2 vinhos, 4 batidas'));
+
 
 // =================================================================
