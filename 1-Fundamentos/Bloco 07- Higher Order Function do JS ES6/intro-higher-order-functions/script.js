ba6deb8de10a844a8dce23131aaf2127487eda3e
josi-lima <teacher.josi@yahoo.com>
2023-03-02 16:13:18 -0300

    :books: feat: Update JS file HOFs

---
+++ 1-Fundamentos/Bloco 07- Higher Order Function do JS ES6/intro-higher-order-functions/script.js
@@ -0,0 +1,15 @@
+// ======= *** HIGHER ORDER FUNCTIONS *** =============================================
+
+/*🚀 1. Use o método forEach para exibir a lista de emails com a seguinte frase: O email ${email} está cadastrado em nosso banco de dados!. */
+
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+emailListInData.forEach(email => console.log(`O email ${email} está cadastrado em nosso banco de dados!`));
+
+// ======================================================================================
+
