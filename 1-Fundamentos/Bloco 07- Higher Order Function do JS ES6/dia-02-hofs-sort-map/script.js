b8fcedc8637ad7572d580196ea8f044bad180389
josi-lima <teacher.josi@yahoo.com>
2023-03-07 21:55:45 -0300

    :books: feat: Update exercises HOFs

--- 1-Fundamentos/Bloco 07- Higher Order Function do JS ES6/dia-02-hofs-sort-map/script.js
+++ 1-Fundamentos/Bloco 07- Higher Order Function do JS ES6/dia-02-hofs-sort-map/script.js
@@ -17,12 +17,44 @@ const employees = [
 const sortAgesPlus = (arr) => {
   return arr.sort((personA, personB) => personA.age - personB.age);
 }
-console.log(sortAges(employees));
+console.log(sortAgesPlus(employees));
 
 // b) Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
 
 const sortAgesLess = (arr) => arr.sort((personA, personB) => personB.age - personA.age);
-console.log(sortAges(employees));
+console.log(sortAgesLess(employees));
 
-// ===================================================
+// =============== Spread operator ======================================
+
+const greatestFoodsEver = {
+  sweet: 'rice pudding',
+  salty: 'lasagna',
+  beverage: 'chocolate milk',
+};
+
+console.log(...Object.keys(greatestFoodsEver));  // sweet salty beverage
+console.log(...Object.values(greatestFoodsEver));  // rice pudding lasagna chocolate milk
+console.log(...Object.entries(greatestFoodsEver)); // [ 'sweet', 'rice pudding' ] [ 'salty', 'lasagna' ] [ 'beverage', 'chocolate milk' ]
+
+// =============== Object Destructuring ==================================
+
+const nextLanguagesToLearn = {
+  first: 'German',
+  second: 'Japanese',
+  third: 'Dutch'
+}
+
+const { first, second, third } = nextLanguagesToLearn;
+
+console.log(`Next languages to learn: ${first}, ${second} and ${third}.`); 
+// Next languages to learn: German, Japanese and Dutch.
+
+// ---- OR without spread operator --------------------------------
+
+console.log(`Next languages to learn: ${nextLanguagesToLearn.first}, ${nextLanguagesToLearn.second} and ${nextLanguagesToLearn.third}.`); 
+
+// =============== .reduce() ==================================
+
+const concatWord = ['l', 'o', 'v', 'e'].reduce((a, b) => a + b);
+console.log(`The word is: ${concatWord}.`);
 
